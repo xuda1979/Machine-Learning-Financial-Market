a53b4922dc810d870a4a5782fdf7149c5035d9b9
David <exutechnology@gmail.com>
2017-03-11 15:42:22 -0500

    initial files

--- DownloadR3000.m
+++ DownloadR3000.m
@@ -1,7 +1,7 @@
 clear;
  
 
-path='C:\Users\eddy\Documents\GitHub\Matlab\russell_3000_2011-06-27.csv';
+path='russell_3000_2011-06-27.csv';
 file=fopen(path);
 symbols=textscan(file,'%s','delimiter', ',');
  
@@ -18,15 +18,15 @@ yue=yue-1;
 nian=year(date);
  
 symbol='^GSPC';
-URL=['http://chart.finance.yahoo.com/table.csv?s=' symbol '&a=0&b=1&c=1980&d=' int2str(yue) '&e=' int2str(d) '&f=' nian '&g=d&ignore=.csv'];
-path=['C:\Users\eddy\Documents\GitHub\Matlab\historical\' symbol '.csv'];
+URL=['http://chart.finance.yahoo.com/table.csv?s=' symbol '&a=0&b=1&c=1980&d=' int2str(yue) '&e=' int2str(d) '&f=' int2str(nian) '&g=d&ignore=.csv'];
+path=['../historical/' symbol '.csv'];
 urlwrite(URL, path); 
 
 for i=1:zongshu    
 try
 symbol=char(symbols(i,1));
-URL=['http://chart.finance.yahoo.com/table.csv?s=' symbol '&a=0&b=1&c=1980&d=' int2str(yue) '&e=' int2str(d) '&f=' nian '&g=d&ignore=.csv'];
-path=['C:\Users\eddy\Documents\GitHub\Matlab\historical\' symbol '.csv'];
+URL=['http://chart.finance.yahoo.com/table.csv?s=' symbol '&a=0&b=1&c=1980&d=' int2str(yue) '&e=' int2str(d) '&f=' int2str(nian) '&g=d&ignore=.csv'];
+path=['../historical/' symbol '.csv'];
 urlwrite(URL, path); 
 catch
 end
